db2f9ea4d5be6f2488be1d68a5bad1add6dcb1f5
Pranjal Verma <verma.pranjal2804@gmail.com>
2026-08-22 18:09:02 +0530

    Time: 0 ms (100%), Space: 26.6 MB (32.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -13,16 +13,10 @@ public:
         return true;
     }
     bool validPalindrome(string s) {
-        string word="";
-        for(auto x:s ){
-            if(isalpha(x) || (x>='0' && x<='9')){
-                word+= (char)tolower(x);
-            }
-        }
-        int l=0,r=word.length()-1;
+        int l=0,r=s.length()-1;
         while(l<=r){
-            if(word[l]!=word[r]){
-                return check(word,l+1,r) || check(word,l,r-1);
+            if(s[l]!=s[r]){
+                return check(s,l+1,r) || check(s,l,r-1);
             }else{
                 r--;
                 l++;
