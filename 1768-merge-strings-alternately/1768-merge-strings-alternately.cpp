af99799af06579f6a60c759fefbb69cbd4b43a9d
Pranjal Verma <verma.pranjal2804@gmail.com>
2026-08-22 23:23:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.69%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int length1=word1.length();
-        int length2=word2.length();
-        int length=0,max_length=0;
-        bool c=false;
-        if(length1==length2){
-            length=length1;
-            c=true;
-        }else{
-            length=min(length1,length2);
-            max_length=max(length1,length2);
-        }
-        string answer="";
-        for(int i=0;i<length;i++){
+       string answer="";
+       int i=0;
+       while(i<word1.length() || i<word2.length()){
+        if(i<word1.length()){
             answer+= word1[i];
+        }
+        if(i<word2.length()){
             answer+= word2[i];
         }
-        if(c){
-            return answer;
-        }else{
-            string x = word1.substr(length,max_length-length);
-            string y = word2.substr(length,max_length-length);
-            cout << x << y ;
-            answer.append(x);
-            answer.append(y);
-            return answer;
-        }
+        i++;
+       }
+       return answer;
     }
 };
